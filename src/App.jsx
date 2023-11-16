94ab9a8620543600d8deb50ac784ee120e3bfea6
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-11-16 21:43:30 +0100

    createFolders

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
-import SharedLayout from './components/SharedLayout/SharedLayout';
-import FirstPage from './pages/FirstPage/FirstPage';
-import SecondPage from './pages/SecondPage/SecondPage';
-import HalfPage from './pages/HalfPage/HalfPage';
-import ErrorPage from './pages/ErrorPage/ErrorPage';
-import { AppWrapper } from './App.styled';
 
-const test = import.meta.env.VITE_API_TEST;
+// const test = import.meta.env.VITE_API_TEST;
 
 function App() {
-  console.log(test);
   return (
-    <AppWrapper>
+    <>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
+        <Route path="/" element={<p>layout</p>}>
+          <Route path="/first" element={<p>current page</p>} />
+          <Route path="/second" element={<p>second page</p>}>
+            <Route path=":half" element={<p>half</p>} />
           </Route>
 
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="*" element={<p>not found</p>} />
         </Route>
       </Routes>
-    </AppWrapper>
+    </>
   );
 }
 export default App;
