6693cb973d264ee17793c3d7ff4e09ba6a2f0bc6
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-11-19 17:04:21 +0200

    +

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,15 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header/header';
+import { VisuallyHidden } from '../../ui';
 
 export default function SharedLayout() {
   return (
-    <>
+    <VisuallyHidden>
       <Header />
       <main>
         <Suspense fallback={<h2>Loading...</h2>}>{<Outlet />}</Suspense>
       </main>
-    </>
+    </VisuallyHidden>
   );
 }
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,15 @@
 import { Container } from './Container/Container.jsx';
 import { Title } from './Title/Title.jsx';
+import { VisuallyHidden } from './VisuallyHidden/VisuallyHidden.jsx';
 import {
   CategoryButton,
   CategoryButtonActive,
 } from './CategoryButton/CategoryButton.jsx';
 
-export { Container, Title, CategoryButton, CategoryButtonActive };
+export {
+  Container,
+  Title,
+  CategoryButton,
+  CategoryButtonActive,
+  VisuallyHidden,
+};
