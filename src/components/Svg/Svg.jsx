e483fc4db96d02d83733ae3482878821f97d1691
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-11-21 20:35:18 +0100

    fixed-svg

--- src/components/Svg/Svg.jsx
+++ src/components/Svg/Svg.jsx
@@ -2,7 +2,7 @@ import sprite from '../../images/sprite.svg';
 
 const Svg = ({ id, size, fill, stroke, className = '', onClick }) => {
   return (
-    <div className={className} onClick={onClick}>
+    <div onClick={onClick}>
       <svg width={size} height={size} fill={fill} stroke={stroke}>
         <use href={`${sprite}#${id}`} />
       </svg>
