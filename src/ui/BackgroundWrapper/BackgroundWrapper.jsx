32d6752058656626616e76f59e04e6d38b5c2172
Nikolay Shein <qseboy1998@gmail.com>
2023-11-24 16:22:48 +0200

    add min-w for auth forms, fix horizontal scroll on reg and login pages

--- src/ui/BackgroundWrapper/BackgroundWrapper.jsx
+++ src/ui/BackgroundWrapper/BackgroundWrapper.jsx
@@ -1,7 +1,7 @@
 const BackgroundWrapper = ({ children, padding }) => {
   return (
     <div
-      className={`w-[100vw] h-[100vh] ${padding} bg-background bg-[url('src/images/mobile_bg.png')] xl:bg-[url('src/images/desktop_bg.png')] md:bg-[url('src/images/tablet_bg.png')] bg-center bg-cover bg-no-repeat`}
+      className={`h-[100vh] ${padding} bg-background bg-[url('src/images/mobile_bg.png')] xl:bg-[url('src/images/desktop_bg.png')] md:bg-[url('src/images/tablet_bg.png')] bg-center bg-cover bg-no-repeat`}
     >
       {children}
     </div>
