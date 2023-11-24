d44c4a9f7356f449a0d7acd86a09087aea525160
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-11-24 18:16:24 +0200

    фікс багів, стилі при ховері, вихід з акаунту,  валідація

--- src/components/UserCard/UserForm.jsx
+++ src/components/UserCard/UserForm.jsx
@@ -5,29 +5,26 @@ import { PhotoIcon } from '../PhotoIcon/PhotoIcon';
 import { useFormik } from 'formik';
 import Svg from '../Svg/Svg';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUser, selectAuth } from '../../Redux/auth/auth-selectors';
+import { getUser } from '../../Redux/auth/auth-selectors';
 import { update } from '../../Redux/auth/auth-operations';
 import { userSchema } from './UserSchema';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { format, parseISO } from 'date-fns';
 
-const errorMessageStyles =
-  'absolute -bottom-[18px] ml-4 text-red text-xs font-normal';
-
+const errorTextStyle ="pl-4 absolute -bottom-5 text-rose-500 text-xs font-normal top-6 left-[60px] xl:left-[85px]"
+const hoverStyle = "transition duration-200 ease-in-out cursor-pointer hover:opacity-80"
 const labelStyle =
   "text-neutral-900 text-sm font-semibold font-['Manrope'] tracking-wide mdOnly:text-[16px] ";
 const inputStyle =
   "text-neutral-900 text-xs font-normal font-['Manrope'] tracking-wide w-[190px] h-6 px-3 py-1 rounded-[20px] border border-blue-400 justify-start items-center gap-[191px] inline-flex md:w-[255px]  xl:w-[255px]";
 
-export const UserForm = () => {
+export const UserForm = ({onTogleLeavingModal}) => {
   const user = useSelector(getUser);
   const dispatch = useDispatch();
 
   const [avatarFile, setAvatarFile] = useState(null);
   const [isEdit, setIsEdit] = useState(false);
 
-  const [userAvatar, setUserAvatar] = useState(user.avatarURL);
   const [previewAvatar, setPreviewAvatar] = useState(null);
   const [changeAvatar, setChangeAvatar] = useState(false);
   const [confirmChangeAvatar, setConfirmChangeAvatar] = useState(false);
@@ -38,21 +35,29 @@ export const UserForm = () => {
     }
   }, [isEdit]);
 
-  console.log(user.birthday);
+  const birthdayType = (date) => {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+    const dateString = "2023-11-07T22:00:00.000Z";
+  
+    if (typeof date === 'string' && dateRegex.test(date)) {
+      return date;
+    }
 
+    return Date.now()
+  };
 
 
   const formik = useFormik({
     initialValues: {
       name: user.name || '',
       email: user.email || '',
-      birthday: user.birthday || '',
+      birthday: birthdayType(user.birthday),
       phone: user.phone || '',
       city: user.city || '',
     },
     validateOnChange: false,
-    validateOnBlur: false,
-    // validationSchema: userSchema,
+    validateOnBlur: true,
+    validationSchema: userSchema,
 
     onSubmit: ({ name, email, birthday, phone, city }) => {
       const updateUser = {
@@ -71,11 +76,10 @@ export const UserForm = () => {
       }
 
       const formData = createUserFormData(updateUser);
-
       dispatch(update(formData));
-      setUserAvatar(previewAvatar);
       setChangeAvatar(false);
       setIsEdit(false);
+  
     },
   });
 
@@ -128,9 +132,9 @@ export const UserForm = () => {
       className="flex flex-col flex-wrap-reverse"
       onSubmit={formik.handleSubmit}
     >
-      <div className="absolute top-[14px] right-[14px]">
+      <div className={`absolute top-[14px] right-[14px] ${hoverStyle}`}>
         {!isEdit ? (
-          <div onClick={() => setIsEdit(true)}>
+          <div  onClick={() => setIsEdit(true)}>
             <EditIcon />
           </div>
         ) : (
@@ -150,7 +154,7 @@ export const UserForm = () => {
             <div className="flex justify-center mb-[14px]">
               <img
                 className="w-[182px] h-[182px] rounded-[40px] object-cover"
-                src={previewAvatar ? previewAvatar : userAvatar}
+                src={previewAvatar ? previewAvatar : user.avatarURL}
                 alt="User Avatar"
               />
             </div>
@@ -158,7 +162,7 @@ export const UserForm = () => {
               <div
                 className={
                   isEdit
-                    ? 'flex flex-row justify-center gap-[5px] mb-[21px]'
+                    ? `flex flex-row justify-center gap-[5px] mb-[21px] ${hoverStyle}`
                     : 'flex flex-row justify-center gap-[5px] mb-[21px] opacity-0'
                 }
               >
@@ -179,6 +183,7 @@ export const UserForm = () => {
                     size={24}
                     stroke={'#54ADFF'}
                     fill={'transparent'}
+                    className={hoverStyle}
                   />
                 </div>
                 Confirm
@@ -194,6 +199,7 @@ export const UserForm = () => {
                     size={24}
                     stroke={'#54ADFF'}
                     fill={'transparent'}
+                    className={hoverStyle}
                   />
                 </div>
               </div>
@@ -220,7 +226,7 @@ export const UserForm = () => {
         {/* All text-fields */}
         <div className="flex flex-col gap-[20px]">
           {/* Name */}
-          <div className="flex justify-between">
+          <div className="flex justify-between relative">
             <label className={labelStyle} htmlFor="name">
               Name:
             </label>
@@ -234,19 +240,19 @@ export const UserForm = () => {
               readOnly={!isEdit}
             />
             {errors['name'] && (
-              <p className="pl-4 absolute -bottom-5 text-rose-500 text-xs font-normal">
+              <p className={errorTextStyle}>
                 {errors['name']}
               </p>
             )}
           </div>
 
           {/* Email */}
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full relative">
             <label className={labelStyle} htmlFor="email">
               Email:
             </label>
             <input
-              className={inputStyle}
+              className={`${inputStyle} ${errors['email'] && 'border-rose-400'}`}
               type="text"
               id="email"
               name="email"
@@ -254,6 +260,11 @@ export const UserForm = () => {
               onChange={formik.handleChange}
               readOnly={!isEdit}
             />
+                        {errors['email'] && (
+              <p className={errorTextStyle}>
+                {errors['email']}
+              </p>
+            )}
           </div>
 
           {/* Birthday */}
@@ -288,23 +299,29 @@ export const UserForm = () => {
           </div>
 
           {/* Phone */}
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full relative">
             <label className={labelStyle} htmlFor="phone">
               Phone:
             </label>
             <input
-              className={inputStyle}
+              className={`${inputStyle} ${errors['phone'] && 'border-rose-400'}`}
               type="tel"
               id="phone"
               name="phone"
+              placeholder='+380000000000'
               value={formikValues['phone']}
               onChange={formik.handleChange}
               readOnly={!isEdit}
             />
+             {errors['phone'] && (
+              <p className={errorTextStyle}>
+              {errors['phone']}
+              </p>
+            )}
           </div>
 
           {/* City */}
-          <div className="flex justify-between">
+          <div className="flex justify-between relative">
             <label className={labelStyle} htmlFor="city">
               City:
             </label>
@@ -318,7 +335,7 @@ export const UserForm = () => {
               readOnly={!isEdit}
             />
             {errors['city'] && (
-              <p className="pl-4 absolute -bottom-5 text-rose-500 text-xs font-normal">
+              <p className={errorTextStyle}>
                 {errors['city']}
               </p>
             )}
@@ -335,14 +352,18 @@ export const UserForm = () => {
               </button>
             </div>
           ) : (
-            <div className="w-full flex text-zinc-500 text-base font-medium font-['Manrope'] tracking-wide gap-[12px]">
-              <Svg
+            <div 
+          onClick={onTogleLeavingModal}
+            className="w-full flex text-zinc-500 text-base font-medium font-['Manrope'] tracking-wide ">
+            <div className={`cursor-pointer hover:opacity-80 flex  gap-[12px] ${hoverStyle}`}>
+            <Svg
                 id={'icon-logout'}
                 size={24}
                 stroke={'#54ADFF'}
                 fill={'transparent'}
               />
               Log Out
+            </div>
             </div>
           )}
         </div>
