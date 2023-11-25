9c7ac9c916e2d111cbe105c118ec6da20cdee4a9
ribacot <remontdiskov1979@gmail.com>
2023-11-25 20:07:00 +0200

    Update BtnLogout.jsx

--- src/components/Header/BtnLogout/BtnLogout.jsx
+++ src/components/Header/BtnLogout/BtnLogout.jsx
@@ -4,13 +4,12 @@ import { firstLetterUp } from '../helpers';
 
 export default function BtnLogout({ children, style, onClick }) {
   return (
-    <NavLink
-      to="/"
+    <button
       className={`flex w-[165px] px-[20px] py-[8px] justify-center items-center gap-[8px] border-[2px]  rounded-[40px] cursor-pointer} bg-blue border-blue text-white hover:blue-gradient ${style} `}
       onClick={onClick}
     >
       <span>{firstLetterUp(children)}</span>
       <Svg size="24px" id="icon-logout" stroke="white" fill="transparent" />
-    </NavLink>
+    </button>
   );
 }
