529e77efd11faeaec41f207945fcd4695a4bb181
KseniyaPin <kontr.mat@gmail.com>
2023-11-25 15:03:44 +0100

    home-and-Not found-fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,16 +2,34 @@ import { Container } from '../../ui/Container/Container';
 
 const MainPage = () => {
   return (
-    <div
-      className="bg-main-page-mobi h-screen bg-no-repeat bg-cover bg-bottom 
-    md:bg-center md:bg-contain md:bg-main-page-tablet 
-    xl:bg-top xl:bg-main-page-desctop"
-    >
+    <div>
       <Container>
         <h1 className="w-[280px] text-black text-[32px] mt-[44px] font-bold font-['Manrope'] leading-[44px] md:w-[588px] md:text-[68px] md:mt-[88px] md:leading-[100px] xl:w-[501px] xl:text-neutral-900  xl:font-extrabold xl:leading-[88.40px]">
           Take good care of your small pets
         </h1>
       </Container>
+      <picture className="relative ">
+        <source
+          srcset="/src/images/main_img/main.png"
+          media="(max-width: 767px)"
+          className="min-w-[520px]  mt-8 -ml-24"
+        />
+        <source
+          srcset="/src/images/main_img/main@2x.png"
+          media="(min-width: 768px)"
+          className="min-w-[900px] -ml-16 -mt-20"
+        />
+        <source
+          srcset="/src/images/main_img/main@3x.png"
+          media="(min-width: 1280px)"
+          className="absolute w-[680px] -mt-[420px] ml-72"
+        />
+        <img
+          src="/src/images/main_img/main@3x.png"
+          alt="Сute dogs and cat"
+          className="min-w-[520px]  mt-8 -ml-24 md:min-w-[900px] md:-ml-16 md:-mt-20 xl:absolute xl:w-[680px] xl:-mt-[420px] xl:ml-72"
+        />
+      </picture>
     </div>
   );
 };
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,8 @@
 import ToMainPageButton from '../../components/ToMainPageButton/ToMainPageButton';
 
 const NotFoundPage = () => {
-  
   return (
-    <div
-      className={`bg-not-found-page-mobi bg-no-repeat bg-center h-[400px] md:bg-not-found-page-tablet md:h-[600px] xl:bg-not-found-page-desctop  xl:h-[480px]`}
-    >
+    <div>
       <div
         className="block w-screen text-center text-black text-2xl font-bold font-['Manrope'] mt-[64px]
 		md:flex md:place-content-center md:w-[768px] md:text-neutral-900 md:text-[32px] md:font-smibold md:leading-[41.60px] xl:w-[1280px] xl:text-[32px] xl:mt-[44px] "
@@ -13,7 +10,30 @@ const NotFoundPage = () => {
         <span> Ooops!</span>
         <h2>This page not found :(</h2>
       </div>
-      <ToMainPageButton  />
+
+      <picture>
+        {/* <source
+          srcset="/src/images/not_found_page/not_found_page_mobi.png"
+          media="(max-width: 767px)"
+          className="object-cover min-w-[280px] object-center"
+        />
+        <source
+          srcset="/src/images/not_found_page/not_found_page_tablet.png"
+          media="(min-width: 768px)"
+          className="object-cover w-[704px] object-center"
+        />
+        <source
+          srcset="/src/images/not_found_page/not_found_page_desctop.png"
+          media="(min-width: 1280px)"
+          className="object-cover w-[822px] object-center"
+        /> */}
+        <img
+          src="/src/images/not_found_page/not_found_page_desctop.png"
+          alt="Сute dogs and cat"
+          className=" mt-11   min-w-[280px] md:min-w-[704px] "
+        />
+      </picture>
+      <ToMainPageButton />
     </div>
   );
 };
