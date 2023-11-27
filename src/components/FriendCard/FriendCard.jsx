6d57d643887e6d6963609d10ccafec144557f244
ribacot <remontdiskov1979@gmail.com>
2023-11-27 14:01:50 +0200

    upd time Work

--- src/components/FriendCard/FriendCard.jsx
+++ src/components/FriendCard/FriendCard.jsx
@@ -1,37 +1,38 @@
 import { FriendsItem } from './FriendsItem';
 
 const FriendCard = ({ friends }) => {
-  const friendCard = friends.map(
-    ({
-      _id,
-      title,
-      url,
-      addressUrl,
-      imageUrl,
-      address,
-      workDays,
-      phone,
-      email,
-    }) => (
-      <FriendsItem
-        key={_id}
-        title={title}
-        siteUrl={url}
-        mapUrl={addressUrl}
-        imageUrl={imageUrl}
-        adress={address}
-        workDays={workDays}
-        phone={phone}
-        email={email}
-      />
-    ),
-  );
+  const friendCards = friends.map((friend) => <FriendsItem key={friend._id}
+  friend={friend} />);
+  // const friendCards = friends.map(
+  //   ({
+  //     _id,
+  //     title,
+  //     url,
+  //     addressUrl,
+  //     imageUrl,
+  //     address,
+  //     workDays,
+  //     phone,
+  //     email,
+  //   }) => (
+  //     <FriendsItem
+  //       key={_id}
+  //       title={title}
+  //       siteUrl={url}
+  //       mapUrl={addressUrl}
+  //       imageUrl={imageUrl}
+  //       adress={address}
+  //       workDays={workDays}
+  //       phone={phone}
+  //       email={email}
+  //     />
+  //   ),
+  // );
 
   return (
     <>
       <ul className="grid gap-[20px] md:grid-cols-2 md:gap-[32px] xl:md:grid-cols-3">
-        {friendCard}
-        {friendCard}
+        {friendCards}
       </ul>
     </>
   );
--- src/components/FriendCard/FriendsItem.jsx
+++ src/components/FriendCard/FriendsItem.jsx
@@ -1,20 +1,16 @@
 const dayNow = new Date();
-const numberOfDay = dayNow.getDay();
+const deyWeekArr = [6, 0, 1, 2, 3, 4, 5];
 
-export const FriendsItem = ({
-  title,
-  siteUrl,
-  mapUrl,
-  imageUrl,
-  adress,
-  workDays,
-  phone,
-  email,
-}) => {
+const numberOfDay = deyWeekArr[dayNow.getDay()];
+
+export const FriendsItem = ({ friend }) => {
+  const { address, addressUrl, email, imageUrl, phone, title, url, workDays } =
+    friend;
+
   return (
     <li className="px-[16px] py-[12px] w-[280px] h-[239px] bg-white rounded-[40px] shadow md:w-[336px] md:h-[275px] xl:max-w-[395px]">
       <a
-        href={siteUrl}
+        href={url}
         target="_blank"
         rel="noreferrer noopener nofollow"
         className="block mb-[16px] font-bold text-center text-blue-400 text-xl font-['Manrope'] "
@@ -34,9 +30,13 @@ export const FriendsItem = ({
                 Time:
               </p>
               <p className="cursor-pointer flex items-center p-0 border-none text-blue-400 font-medium font-['Manrope'] text-xs w-[85px] md:text-sm md:w-[76px] xl:w-[141px] xl:text-base">
-                {workDays & workDays?.from ? 
-              `${workDays[numberOfDay].from || ""}-${workDays[numberOfDay].to || ""}` : "day and night"}
-                
+                {workDays
+                  ? !workDays[numberOfDay].isOpen
+                    ? 'Close'
+                    : `${workDays[numberOfDay].from || ''}-${
+                        workDays[numberOfDay].to || ''
+                      }`
+                  : 'day and night'}
               </p>
             </div>
           </li>
@@ -46,10 +46,10 @@ export const FriendsItem = ({
             </p>
             <a
               className="no-underline font-medium text-xs md:text-sm xl:text-base"
-              href={mapUrl}
+              href={addressUrl}
               target="_blank"
             >
-              {adress}
+              {address}
             </a>
           </li>
           <li className="flex flex-col items-start mb-[12px] last:mb-0">
