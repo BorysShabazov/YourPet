e5d0a7d2a3853229671c24bb13937226c9d3cd86
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-11-27 06:43:38 +0000

    Deploying to gh-pages from @ BorysShabazov/YourPet@4996a4648407f3a415e47e09ceb5d221bd8d534e 🚀

---
+++ assets/PrivateRoute-d9b000e7.js
@@ -0,0 +1,1 @@
+import{u as o,H as s,j as a,I as r}from"./index-64926d13.js";function n({children:t}){const{token:e}=o(s);return e?t:a.jsx(r,{to:"/login"})}export{n as default};
---
+++ assets/Title-ad8ff3cd.js
@@ -0,0 +1,1 @@
+import{j as x}from"./index-64926d13.js";const r=({className:t="",children:e})=>x.jsx("h2",{className:`text-center text-black text-2xl font-bold manrope md:text-5xl ${t}`,children:e});export{r as T};
