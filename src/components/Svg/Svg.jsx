3fd93c427704648fd037fa1541a20464c8902e0b
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-11-28 20:05:30 +0100

    add-calss-name-and-avatars

--- src/components/Svg/Svg.jsx
+++ src/components/Svg/Svg.jsx
@@ -1,9 +1,23 @@
 import sprite from '../../images/sprite.svg';
 
-const Svg = ({ id, size, fill, stroke, className = '', onClick }) => {
+const Svg = ({
+  id,
+  size,
+  fill,
+  stroke,
+  className = '',
+  onClick,
+  classNameSvg = '',
+}) => {
   return (
     <div onClick={onClick} className={className}>
-      <svg width={size} height={size} fill={fill} stroke={stroke}>
+      <svg
+        className={classNameSvg}
+        width={size}
+        height={size}
+        fill={fill}
+        stroke={stroke}
+      >
         <use href={`${sprite}#${id}`} />
       </svg>
     </div>
