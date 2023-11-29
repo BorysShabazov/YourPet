e9bedab193f80ca499ce247020fae5fb89e547e0
KseniyaPin <kontr.mat@gmail.com>
2023-11-29 14:58:03 +0100

    hover Our Friends --- Oksana

--- src/components/FriendCard/ListItemDescription.jsx
+++ src/components/FriendCard/ListItemDescription.jsx
@@ -11,15 +11,15 @@ export default function ListItemDescription({
   };
 
   return (
-    <li className="flex flex-col items-start">
-      <p className="flex justify-between text-neutral-900  font-semibold font-['Manrope'] text-xs md:text-sm xl:text-base ">
+    <li className="group flex flex-col items-start hover:text-blue ">
+      <p className="flex justify-between text-neutral-900  font-semibold font-['Manrope'] text-xs md:text-sm xl:text-base  group-hover:text-blue">
         {title}
       </p>
       <a
         className="no-underline font-normal text-xs md:text-sm xl:text-base"
         title={title}
         href={url}
-        target={!url.includes('tel' || 'mailto') ? '_blank' : ''}
+        target={!url.includes('tel' || 'mailto' || 'time') ? '_blank' : ''}
         onClick={onClick}
       >
         {children}
