6835f6d78d2541d36cc25da08ba77243e453a664
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-11-29 06:44:56 +0000

    Deploying to gh-pages from @ BorysShabazov/YourPet@b997634e30f074a9f184cc515ea9b600c3adba43 🚀

--- assets/noticesSelectors-0851ff6e.js
+++
@@ -1,1 +0,0 @@
-const t=e=>e.notices.items,o=e=>e.notices.selectedNotice,s=e=>e.notices.isLoading,c=e=>e.notices.error;export{c as a,o as b,t as c,s as g};
---
+++ assets/noticesSelectors-dbeec028.js
@@ -0,0 +1,1 @@
+const e=t=>t.notices.items,o=t=>t.notices.selectedNotice,s=t=>t.notices.isLoading,c=t=>t.notices.error,i=t=>t.notices.total;export{c as a,o as b,e as c,i as d,s as g};
