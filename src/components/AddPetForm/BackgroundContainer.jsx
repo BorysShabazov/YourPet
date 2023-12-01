d15fd5eb12feae44b241ce97f13788415a6acd18
ribacot <remontdiskov1979@gmail.com>
2023-12-01 11:17:37 +0200

    Update BackgroundContainer.jsx

--- src/components/AddPetForm/BackgroundContainer.jsx
+++ src/components/AddPetForm/BackgroundContainer.jsx
@@ -1,7 +1,7 @@
 const BackgroundContainer = ({ children }) => {
   return (
     <div
-      className={`h-full w-full  pt-[68px] pb-[50px] bg-[url('src/images/mobile_bg.png')] xl:bg-[url('src/images/desktop_bg.png')] md:bg-[url('src/images/tablet_bg.png')] bg-center bg-contain bg-no-repeat bg-fixed`}
+      className={`h-full w-full  pt-[68px] pb-[50px] bg-[url('src/images/mobile_bg.png')] xl:bg-[url('src/images/desktop_bg.png')] md:bg-[url('src/images/tablet_bg.png')] bg-center bg-contain bg-no-repeat `}
     >
       {children}
     </div>
