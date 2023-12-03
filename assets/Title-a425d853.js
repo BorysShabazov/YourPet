8bd2bfe97a10275dd28d089f79edfb78f0cbe4ef
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-12-03 18:50:07 +0000

    Deploying to gh-pages from @ BorysShabazov/YourPet@0bd2110ec89b05eba51a1b4714c2a4ec3bc14926 🚀

--- assets/Title-a425d853.js
+++
@@ -1,1 +0,0 @@
-import{j as x}from"./index-e1886e3e.js";const r=({className:t="",children:e})=>x.jsx("h2",{className:`text-center text-black text-2xl font-bold manrope md:text-5xl ${t}`,children:e});export{r as T};
