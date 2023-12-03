30667f727e7737d9df84c1df8c278defa79f640d
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-12-03 19:03:59 +0000

    Deploying to gh-pages from @ BorysShabazov/YourPet@c79b83d784ff4b93d97ac9643c157ddf2cf1c7eb 🚀

--- assets/Title-008c47e4.js
+++
@@ -1,1 +0,0 @@
-import{j as x}from"./index-e9e195a0.js";const r=({className:t="",children:e})=>x.jsx("h2",{className:`text-center text-black text-2xl font-bold manrope md:text-5xl ${t}`,children:e});export{r as T};
---
+++ assets/Title-854c2d51.js
@@ -0,0 +1,1 @@
+import{j as x}from"./index-889ac5c7.js";const r=({className:t="",children:e})=>x.jsx("h2",{className:`text-center text-black text-2xl font-bold manrope md:text-5xl ${t}`,children:e});export{r as T};
